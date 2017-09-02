5e04a072439e148d5b6b68dff38a4a7165f6566b
Ricky Amparo <rickyamparo@gmail.com>
2017-09-02 15:50:59 -0600

    add valid attributes test to comment spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,7 +4,15 @@ describe Comment do
   describe "validations" do
     context "invalid attributes" do
       it "is invalid without content" do
+        comment = Comment.new()
+        expect(comment).to be_invalid
+      end
+    end
+
+    context "valid attributes" do
+      it "is valid with content" do
         comment = create(:comment)
+        expect(comment).to be_valid
       end
     end
   end
