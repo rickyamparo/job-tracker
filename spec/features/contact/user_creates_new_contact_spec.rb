fcfbf57c6158ea12f1288cce267ed73c44ea9166
Ricky Amparo <rickyamparo@gmail.com>
2017-09-03 16:00:03 -0600

    create the contact form partial

---
+++ spec/features/contact/user_creates_new_contact_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe "User creates new comment for job" do
+  scenario "a user uses the comment form to create a new comment" do
+    contact = create(:contact)
+    company = contact.company
+    
+    name = "Turtanator"
+    email = "Schmeckles@gmail.com"
+    position = "Exterminator"
+
+    visit company_path(company)
+
+    fill_in "contact[name]", with: name
+    fill_in "contact[email]", with: email
+    fill_in "contact[position]", with: position
+    click_button "Create"
+
+    expect(current_path).to eq "/companies/#{company.id}"
+    expect(page).to have_content "Turtles can comment too"
+  end
+end
