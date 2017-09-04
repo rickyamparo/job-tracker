268074f7eabe04fdb416727c50c81ea6545534cd
Ricky Amparo <rickyamparo@gmail.com>
2017-09-04 15:57:07 -0600

    test now failing

---
+++ spec/features/dashboard/user_can_see_dashboard_info_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "User Can see all Dashboard Info Correctly" do
+  scenario "a user visits the dashboard to view job count by level of interest" do
+    job1 = create(:job, level_of_interest: 20)
+    job2 = create(:job, level_of_interest: 20, company: job1.company)
+    job3 = create(:job, level_of_interest: 20, company: job1.company)
+    job4 = create(:job, level_of_interest: 10, company: job1.company)
+
+    visit "/dashboard"
+
+    expect(page).to have_content(3)
+    expect(page).to have_content(1)
+  end
+end
